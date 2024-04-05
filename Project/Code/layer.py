e20cf33a14e22602f2de071effb1536a641d723e
Bragit123 <b.a.trefjord@fys.uio.no>
2024-04-05 12:32:27 +0200

    Added abstract Layer class

---
+++ Project/Code/layer.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+class Layer:
+    """
+    Abstract class for layers
+    """
+
+    def __init__(self, seed: int = 100):
+        self.seed = seed
+    
+    # Overwrite if layer has weights
+    def reset_weights(self):
+        pass
+    
+    # Overwrite if layer has schedulers
+    def reset_schedulers(self):
+        pass
+    
+    # Must be overwritten
+    def feed_forward(self, input: np.ndarray):
+        raise NotImplementedError
+
+    # Must be overwritten
+    def backpropagate(self, dC_doutput: np.ndarray, lmbd: float):
+        raise NotImplementedError
+    
